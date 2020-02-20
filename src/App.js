da2889cc2e976c5a2b2d6c07d4da1b518cfe2536
Shawn DelPercio <sdelperc@gmail.com>
2020-02-20 15:53:39 -0500

    consumed product context in products.js; created cartcontext

--- src/App.js
+++ src/App.js
@@ -21,14 +21,8 @@ function App() {
 		<div className='App'>
 			<ProductContext.Provider value={{ products, addItem }}>
 				<Navigation cart={cart} />
-
 				{/* Routes */}
-				<Route
-					exact
-					path='/'
-					render={() => <Products products={products} addItem={addItem} />}
-				/>
-
+				<Route exact path='/' component={Products} />
 				<Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
 			</ProductContext.Provider>
 		</div>

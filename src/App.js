28b08dd1bcf989271238d31be21a089e269abccb
Shawn DelPercio <sdelperc@gmail.com>
2020-02-20 16:00:16 -0500

    created cartcontext provider; consumed cart state from provider in navigation.js and shoppingcart.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -20,10 +21,12 @@ function App() {
 	return (
 		<div className='App'>
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
-				{/* Routes */}
-				<Route exact path='/' component={Products} />
-				<Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+				<CartContext.Provider value={{ cart }}>
+					<Navigation />
+					{/* Routes */}
+					<Route exact path='/' component={Products} />
+					<Route path='/cart' component={ShoppingCart} />
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
